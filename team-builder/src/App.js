ed6e4014e539aeaef92a9d10862533a4ce1dbc67
Phil G <fillupgalbraith@gmail.com>
2021-11-09 11:20:22 -0800

    Added divs

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const initialTeamMembers = [
+    {
+      memberName: "Georgie Porgy",
+      email: "datQueen@porgynpie.com",
+      role: "Broker"
+    },
+    {
+      memberName: "Mary Quite-Contrary",
+      email: "numbaOneHustlah@porgynpie.com",
+      role: "Trend Setter"
+    }
+  ]
+
+  const submit = memberValues => {
+    setTeamMembersList( ...teamMembersList, memberValues )
+  }
+  
+  const [teamMembersList setTeamMembersList] = useState(initialTeamMembers);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Team Management</h1>
+        <p>Assign Team Members</p>
       </header>
+      <div className="newMember">
+      <Label name="NewMember">
+      <h2>NewMember</h2>
+      <form>
+        
+        </Label>
+      </div>
+      <div className="currentMembers">
+        <h2> currentMembers</h2>
+         <MemberList members={teamMembersList}/>
+      </div>
     </div>
   );
 }
